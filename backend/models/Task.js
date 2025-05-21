2e6a0c689ce246aa5dc276fd7cede3400a57f8e8
super <mpared@insdanielblanxart.cat>
2025-05-21 23:45:38 +0200

    Primer commit: estructura inicial del proyecto gestorTareas

---
+++ backend/models/Task.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const TaskSchema = new mongoose.Schema({
+  titulo: {
+    type: String,
+    required: true
+  },
+  descripcion: {
+    type: String
+  },
+  completada: {
+    type: Boolean,
+    default: false
+  }
+});
+
+module.exports = mongoose.model('Task', TaskSchema);
